676433201f53af9e2c3cb0bed15ce2deeeae8811
eavila2504 <eavilaflores@byupathway.edu>
2026-07-09 22:15:50 -0600

    bom

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,12 +1,23 @@
-const input = document.querySelector('#favchap');
-const buton = document.querySelector('buttonn');
-const list = document.querySelector('_________');
 
-const li = document.createElement('li');
+const inputElement = document.querySelector('#favchap');
+const buttonElement = document.querySelector('button');
+const listElement = document.querySelector('#list');
 
-const deleteButton = document.createElement('button');
+buttonElement.addEventListener('click', function() {
+    if (inputElement.value !== '') {
+        const li = document.createElement('li');
+        li.textContent = inputElement.value;
 
-li.textContent = input.value;
-delteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function() {
+            listElement.removeChild(li);
+            inputElement.focus();
+        });
+
+        li.appendChild(deleteButton);
+        listElement.appendChild(li);
+        inputElement.value = '';
+        inputElement.focus();
+    }
+});
