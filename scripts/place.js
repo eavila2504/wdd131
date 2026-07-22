5500ff7a1a27f1d88c518b6dd489736d9899a104
eavila2504 <eavilaflores@byupathway.edu>
2026-07-21 20:50:19 -0600

    update script place

--- scripts/place.js
+++ scripts/place.js
@@ -9,19 +9,5 @@ const windSpeed = 5; // km/h
 
 
 function calculateWindChill(tempC, windKph) {
-  return (
-    13.12 +
-    0.6215 * tempC -
-    11.37 * Math.pow(windKph, 0.16) +
-    0.3965 * tempC * Math.pow(windKph, 0.16)
-  );
-}
-
-
-const windChillEl = document.getElementById("windchill");
-if (temperature <= 10 && windSpeed > 4.8) {
-  const windChill = calculateWindChill(temperature, windSpeed);
-  windChillEl.textContent = `${windChill.toFixed(1)}°C`;
-} else {
-   windChillEl.textContent = `${temperature}°C`;
+  return 13.12 + 0.6215 * tempC - 11.37 * Math.pow(windKph, 0.16) + 0.3965 * tempC * Math.pow(windKph, 0.16);
 }
