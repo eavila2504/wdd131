e8f7c351ff508feaafb79d4e5786fb55f9d06889
eavila2504 <eavilaflores@byupathway.edu>
2026-08-01 02:38:27 -0600

    form activity

---
+++ week05/scripts/form.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const yearEl = document.getElementById("currentyear");
+    if (yearEl) {
+        yearEl.textContent = new Date().getFullYear();
+    }
+
+    const modifiedEl = document.getElementById("lastModified");
+    if (modifiedEl) {
+        modifiedEl.textContent = `Last Modified: ${document.lastModified}`;
+    }
+});
